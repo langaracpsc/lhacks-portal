aebba7062145a8a7b8c6fe92a9ee27d9fb6478da
Pawel Michalski <pawel230401@gmail.com>
2024-07-06 00:45:26 +0200

    db.py fixed by adding load_environment_variables()

--- backend/lhacks/db.py
+++ backend/lhacks/db.py
@@ -3,6 +3,10 @@ from os import environ as env
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session, sessionmaker
 
+from lhacks.config import load_environment_variables
+
+load_environment_variables()
+
 engine = create_engine(env.get("CONNECTION_STRING"), echo=True)
 
 dbSession = Session(engine)
