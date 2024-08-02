7e3b93c017829880912ce1ca86645560a8e6671b
rishit-singh <rsrishitsingh@gmail.com>
2024-08-02 04:31:31 -0700

    Fixed nav bar closing bug

--- frontend/src/app/header.tsx
+++ frontend/src/app/header.tsx
@@ -10,22 +10,17 @@ import { usePathname } from 'next/navigation'
 export default function Header() {
     const pathname = usePathname()
 
-
-    
-   
-
     const [displayCheck, setDisplayCheck] = useState(true);
     const [isOpen, setIsOpen] = useState(false);
 
-    useEffect(()=>{
-        console.log(isOpen)
-    },[isOpen])
+    // useEffect(() => { // why?
+    //     console.log(isOpen)
+    // }, [isOpen])
     const toggleNavBar = () => {
         setIsOpen(!isOpen);
     };
 
-    const router = useRouter()
-
+    const router = useRouter();
 
     useEffect(() => {
         const handleResize = () => {
@@ -36,7 +31,7 @@ export default function Header() {
                 setDisplayCheck(true);
             }
         };
-    
+
 
         handleResize();
 
@@ -45,83 +40,69 @@ export default function Header() {
         return () => window.removeEventListener('resize', handleResize);
     }, []);
 
-    function redirectAboutUs() {
-        router.push("/schedule")
-    }
-    function redirectMain() {
-        router.push("/")
-    }
-    function redirectHandbook() {
-        router.push("/Handbook")
 
-    }
-
-    function specialAnimationHandle(whichEnd:string){
+    function specialAnimationHandle(whichEnd: string) {
         toggleNavBar()
-        if(pathname == "/"){
-            
+        if (pathname == "/") {
+
             router.push("#" + whichEnd)
             router.refresh()
 
-            
-        }else{
+
+        } else {
             router.push("#" + whichEnd)
 
         }
-        
-
     }
 
 
-   
-    
-
     // picture not full screen as it was
     return (
-
         <>
+            <div className={`z-20 bg-black w-screen     h-screen absolute  ${isOpen ? "visible animate-slide-in  overflow-y-hidden " : "hidden "}`}>
+                <div className={"  flex flex-col w-screen  items-start justify-center gap-8 "}>
+                    <p className="p-4 text-2xl">Log on as:</p>
+                    <div className={"border-b-2 hover:border-orange-500 [&>*]:hover:text-orange-500"}>
+                        <button onClick={() => { 
+                            setIsOpen(false);
+                            router.push("/CheckinPage");
+                        }} className={"font-bold hover:text-orange-500 text-white text-3xl px-6 py-2 bg-none rounded"}>
+                            My Ticket
+                        </button>
+                    </div>
+                    <div className="border-b-2 hover:border-orange-500 [&>*]:hover:text-orange-500">
+                        <button onClick={() => { 
+                            setIsOpen(false);
+                            router.push("/Timer");
+                        }} className="font-bold text-3xl px-6 py-2 bg-none hover:text-orange-500 text-white rounded">
+                            Schedule
+                        </button>
+                    </div>
+                    <div className="border-b-2 hover:border-orange-500 [&>*]:hover:text-orange-500">
+                        <button onClick={() => { 
+                            setIsOpen(false);
+                            router.push("/foodSection");
+                            }} className="font-bold text-3xl px-6 py-2 bg-none hover:text-orange-500 text-white rounded">
+                            Meals
+                        </button>
+                    </div>
+                    <div className="border-b-2 hover:border-orange-500 [&>*]:hover:text-orange-500">
+                        <button onClick={() => { 
+                            setIsOpen(false);
+                            router.push("/Sponsors");
+                        }} className="font-bold text-3xl px-6 py-2 bg-none hover:text-orange-500 text-white rounded">
+                            Sponsors
+                        </button>
+                    </div>
+                </div>
 
 
-            <div className={`z-20 bg-black w-screen     h-screen absolute  ${isOpen?"visible animate-slide-in  overflow-y-hidden ":"hidden "}` }>
+            </div>
 
-
-
-            <div className={"  flex flex-col w-screen  items-start justify-center gap-8 "}>
-      <p className="p-4 text-2xl">Log on as:</p>
-      <div className={"border-b-2 hover:border-orange-500 [&>*]:hover:text-orange-500"}>
-        <button onClick={()=>{router.push("/CheckinPage")}} className={"font-bold hover:text-orange-500 text-white text-3xl px-6 py-2 bg-none rounded"}>
-          My Ticket
-        </button>
-        </div>
-        <div className="border-b-2 hover:border-orange-500 [&>*]:hover:text-orange-500">
-        <button onClick={()=>{router.push("/Timer")}} className="font-bold text-3xl px-6 py-2 bg-none hover:text-orange-500 text-white rounded">
-          Schedule
-        </button>
-      </div>
-      <div className="border-b-2 hover:border-orange-500 [&>*]:hover:text-orange-500">
-        <button onClick={()=>{router.push("/foodSection")}} className="font-bold text-3xl px-6 py-2 bg-none hover:text-orange-500 text-white rounded">
-          Meals
-        </button>
-      </div>
-      <div className="border-b-2 hover:border-orange-500 [&>*]:hover:text-orange-500">
-        <button onClick={()=>{router.push("/Sponsors")}} className="font-bold text-3xl px-6 py-2 bg-none hover:text-orange-500 text-white rounded">
-          Sponsors
-        </button>
-      </div>
-    </div>
-
-
-           </div>
-
-            <div className={`  ${isOpen?"z-20":"z-10"} pt-3 pr-4` }>
+            <div className={`  ${isOpen ? "z-20" : "z-10"} pt-3 pr-4`}>
                 <Hamburger color='white' toggled={isOpen} toggle={setIsOpen} />
             </div>
-           
-        
-        
-            
-        
+
         </>
-        
     );
 }
