cbba0c7a2298c0739ed6b2c55a645b51092951e5
rishit-singh <rsrishitsingh@gmail.com>
2024-09-16 21:09:25 -0700

    Fixed UserID size in MealToken model

--- backend/lhacks/schema/meal.py
+++ backend/lhacks/schema/meal.py
@@ -3,19 +3,21 @@ from sqlalchemy import Column, String, Integer, Boolean
 
 from enum import Enum
 
+
 class MealType(Enum):
     Breakfast = 0
     Lunch = 1
     Dinner = 2
     Snacks = 3
 
+
 class Meal(Base):
     __tablename__ = "meals"
 
     ID = Column(String(36), name="id", primary_key=True, nullable=False)
     Name = Column(String(64), name="name", unique=True, nullable=False)
-    CreatedAt = Column(Integer(), name="created_at",  nullable=False)
-    UpdatedAt = Column(Integer(), name="updated_at", nullable=False) 
+    CreatedAt = Column(Integer(), name="created_at", nullable=False)
+    UpdatedAt = Column(Integer(), name="updated_at", nullable=False)
     Active = Column(Boolean(), name="active", nullable=False)
     Type = Column(Integer(), name="type", nullable=False)
 
@@ -26,7 +28,7 @@ class Meal(Base):
             "created_at": self.CreatedAt,
             "updated_at": self.UpdatedAt,
             "active": self.Active,
-            "type": self.Type
+            "type": self.Type,
         }
 
     def __repr__(self):
@@ -34,12 +36,12 @@ class Meal(Base):
             f"<Meal(id='{self.ID}', name='{self.Name}', "
             f"created_at='{self.CreatedAt}', updated_at='{self.UpdatedAt}', active='{self.Active}', type='{self.Type}')>"
         )
-    
+
 
 class MealToken(Base):
     __tablename__ = "meal_tokens"
     ID = Column(String(36), name="id", primary_key=True, nullable=False)
-    UserID = Column(String(36), name="user_id", nullable=False)  # New field
+    UserID = Column(String(65), name="user_id", nullable=False)  # New field
     MealID = Column(String(36), name="meal_id", nullable=False)
     Used = Column(Boolean(), name="used", nullable=False, default=False)
     UpdatedAt = Column(Integer(), name="updated_at", nullable=False)
@@ -52,7 +54,7 @@ class MealToken(Base):
             "meal_id": self.MealID,
             "used": self.Used,
             "updated_at": self.UpdatedAt,
-            "created_at": self.CreatedAt
+            "created_at": self.CreatedAt,
         }
 
     def __repr__(self):
